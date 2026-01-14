19e87f462c4cb1d1bdf59384b43f7fff9210ed4a
Pankaj-Singh-Nirmal <pk01101991@gmail.com>
2026-01-14 21:29:54 +0100

    chore: scaffold mono-repo and CI skeleton

---
+++ accessguard-audit/src/main/java/io/github/pankajsinghnirmal/accessguard/audit/AccessGuardAuditApplication.java
@@ -0,0 +1,11 @@
+package io.github.pankajsinghnirmal.accessguard.audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessGuardAuditApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccessGuardAuditApplication.class, args);
+    }
+}
---
+++ accessguard-core/src/main/java/io/github/pankajsinghnirmal/accessguard/core/AccessGuardCoreApplication.java
@@ -0,0 +1,11 @@
+package io.github.pankajsinghnirmal.accessguard.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessGuardCoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccessGuardCoreApplication.class, args);
+    }
+}
